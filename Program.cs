76843c3a53cf1bcc7508a7c0a992243e718cf489
Lloyd Cledwyn <cledwyn@gmail.com>
2013-03-12 21:01:33 -0500

    Changed the Encode Line to a subroutine
    To better trap for fileds that contain quotes and such.

--- Program.cs
+++ Program.cs
@@ -60,19 +60,76 @@ namespace OraStoredProcToCSV
                 return;
             }
 
+            string filename = proc.ToLower() + " " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".csv";
+
+            //RKLib.ExportData.Export objExport = new RKLib.ExportData.Export();
+            //objExport.ExportDetails(dt, RKLib.ExportData.Export.ExportFormat.Excel, "Nametag.xls");
+
             StringBuilder sb = new StringBuilder();
 
             IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
                                               Select(column => column.ColumnName);
-            sb.AppendLine("\"" + string.Join("\",\"", columnNames) + "\"");
+            sb.AppendLine(EncodeCsvLine(columnNames));
 
             foreach (DataRow row in dt.Rows)
             {
                 IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
-                sb.AppendLine("\"" + string.Join("\",\"", fields) + "\"");
+                sb.AppendLine(EncodeCsvLine(fields));
             }
 
-            File.WriteAllText(proc.ToLower() + " " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".csv", sb.ToString());
+            File.WriteAllText(filename, sb.ToString());
+        }
+
+        static char DelimiterChar = ',';
+        public static String EncodeCsvLine(IEnumerable<string> fields)
+        {
+            StringBuilder line = new StringBuilder();
+            int i = 0;
+            foreach (string field in fields)
+            {
+                if (i > 0) { line.Append(DelimiterChar); }
+                String csvField = EncodeCsvField(field);
+                line.Append(csvField);
+                i++;
+            }
+            return line.ToString();
+        }
+
+        static String EncodeCsvField(String field)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(field);
+
+            // Some fields with special characters must be embedded in double quotes
+            bool embedInQuotes = false;
+
+            // Embed in quotes to preserve leading/tralining whitespace
+            if (sb.Length > 0 &&
+                (sb[0] == ' ' ||
+                 sb[0] == '\t' ||
+                 sb[sb.Length - 1] == ' ' ||
+                 sb[sb.Length - 1] == '\t')) { embedInQuotes = true; }
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                // Embed in quotes to preserve: commas, line-breaks etc.
+                if (sb[i] == DelimiterChar ||
+                    sb[i] == '\r' ||
+                    sb[i] == '\n' ||
+                    sb[i] == '"')
+                {
+                    embedInQuotes = true;
+                    break;
+                }
+            }
+
+            // If the field itself has quotes, they must each be represented 
+            // by a pair of consecutive quotes.
+            sb.Replace("\"", "\"\"");
+
+            String rv = sb.ToString();
+            if (embedInQuotes) { rv = "\"" + rv + "\""; }
+            return rv;
         }
     }
 }
